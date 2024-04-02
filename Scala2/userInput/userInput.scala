d24e82f4e4b4335ee2932a97dcba0c5a4385a030
Kage Jinsei <dadinho_123@outlook.com>
2024-04-02 11:18:53 -0300

    Fazendo algumas alterações

--- Scala2/userInput/userInput.scala
+++ Scala2/userInput/userInput.scala
@@ -1,6 +1,6 @@
 import scala.io.StdIn.readLine
 
-object helloUser {
+object userInput {
 
   def main(args: Array[String]) = {
     print("Por favor, digite o seu nome: ")
--- Scala3/input.scala
+++
@@ -1,7 +0,0 @@
-import scala.io.StdIn.readLine
-
-@main def helloUser() =
-  print("Por favor, digite o seu nome: ")
-  val name = readLine()
-
-  println("Olá, " + name + "! Seja bem-vindo(a).")
